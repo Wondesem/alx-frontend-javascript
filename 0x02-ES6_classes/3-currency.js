6a80aa437e7bebe40c7d135fccdda44b075f0921
Wondesem <wseminew50@gmail.com>
2023-12-01 03:01:08 -0800

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default function Currency {
+  constructor(code, name) {
+    if(typeof code !== 'string') {
+      throw new TypeError("code must be string");
+    }
+    else if(typeof name !== 'string') {
+      throw new TypeError("name must be string");
+    }
+    this._code = code;
+    this._name = name;
+  }
+  get code() {
+    return this._code;
+  }
+  get name() {
+    return this._name;
+  }
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError(" code must be string');
+    }
+    this._code =code;
+  }
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError("name must be string");
+    }
+    this._name = name;
+  }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
