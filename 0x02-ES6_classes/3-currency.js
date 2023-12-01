22d69184e300effa8af1e6109b91e94eee3b7460
Wondesem <wseminew50@gmail.com>
2023-12-01 03:04:07 -0800

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,33 +1,38 @@
-export default function Currency {
+export default class Currency {
   constructor(code, name) {
-    if(typeof code !== 'string') {
-      throw new TypeError("code must be string");
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    else if(typeof name !== 'string') {
-      throw new TypeError("name must be string");
-    }
+
     this._code = code;
     this._name = name;
   }
-  get code() {
-    return this._code;
-  }
+
   get name() {
     return this._name;
   }
-  set code(code) {
-    if (typeof code !== 'string') {
-      throw new TypeError(" code must be string');
-    }
-    this._code =code;
+
+  get code() {
+    return this._code;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
-      throw new TypeError("name must be string");
+      throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
   displayFullCurrency() {
-    return `${this._name} (${this._code})`
+    return `${this._name} (${this._code})`;
   }
 }
