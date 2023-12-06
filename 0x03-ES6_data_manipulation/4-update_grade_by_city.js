72619477100af054aabf3d5118f8511ff1aec7f1
Wondesem <wseminew50@gmail.com>
2023-12-06 01:07:16 -0800

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,3 @@
 export default function updateStudentGradeByCity(student, city, newGrades) {
-  return students.filter((obj) => obj.location === city).map((obj) => ({...obj, grade: newGrade.filter((gr) => gr.studentId = obj.id).map((gr) => gr.id)[0] || 'N/A'}))
+  return students.filter((obj) => obj.location === city).map((obj) => ({...obj, grade: newGrade.filter((gr) => gr.studentId === obj.id).map((gr) => gr.id)[0] || 'N/A'}))
 }
